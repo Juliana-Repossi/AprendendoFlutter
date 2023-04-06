ef5e5e287833d99536b4ed0da4ec78b55a5eb0a3
Juliana Repossi <julianarepossi@hotmail.com>
2023-04-05 21:31:17 -0300

    modulo 23 a 25

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -27,33 +27,88 @@ class _LoginPageState extends State<LoginPage> {
                   width: 300,
                   height: 180,
                   child: Image.asset('assets/imagens/Logo_Branca.png')),
-              SizedBox(height: 50.0),
-              TextField(
-                onChanged: (value) {
-                  email = value;
-                },
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                    labelText: 'E-mail:', border: OutlineInputBorder()),
+              SizedBox(height: 20.0),
+              Card(
+                color: Color.fromRGBO(255, 255, 255, 0.9),
+                child: Padding(
+                  padding: const EdgeInsets.all(0.8),
+                  child: Column(
+                    children: [
+                      TextField(
+                        onChanged: (value) {
+                          email = value;
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                            labelText: 'E-mail:',
+                            border: UnderlineInputBorder()),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              SizedBox(height: 12),
-              TextField(
-                onChanged: (value) {
-                  password = value;
-                },
-                obscureText: true,
-                decoration: InputDecoration(
-                    labelText: 'Password:', border: OutlineInputBorder()),
+              Card(
+                color: Color.fromRGBO(255, 255, 255, 0.9),
+                child: Padding(
+                  padding: const EdgeInsets.all(0.8),
+                  child: Column(
+                    children: [
+                      TextField(
+                        onChanged: (value) {
+                          password = value;
+                        },
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            labelText: 'Password:',
+                            border: UnderlineInputBorder()),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              SizedBox(height: 15),
-              ElevatedButton(
-                onPressed: () {
-                  if (email == "juliana" && password == "123") {
-                    Navigator.of(context).pushReplacementNamed('/home');
-                  }
-                },
-                child: Text('Entrar'),
-              )
+              Column(
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromRGBO(92, 72, 151, 1),
+                      elevation: 20,
+                    ),
+                    onPressed: () {
+                      if (email == "juliana" && password == "123") {
+                        Navigator.of(context).pushReplacementNamed('/home');
+                      }
+                    },
+                    child: SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        height: 40.0,
+                        child: Text(
+                          'Entrar',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                          textAlign: TextAlign.center,
+                        )),
+                  ),
+                  SizedBox(height: 12),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromRGBO(255, 177, 43, 1),
+                      elevation: 10,
+                    ),
+                    onPressed: () {},
+                    child: SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        height: 40.0,
+                        child: Text(
+                          'Cadastre-se',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                          textAlign: TextAlign.center,
+                        )),
+                  )
+                ],
+              ),
             ],
           ),
         ),
